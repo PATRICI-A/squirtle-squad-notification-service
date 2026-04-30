09388ec4f7232f2f9bd7d9893bfbbd8d11706289
CristianGuerrero <cristian.guerrero-b@mail.escuelaing.edu.co>
2026-04-30 13:39:07 -0500

    Feat: Implementacion Enums Tipos de notificaciones

---
+++ src/main/java/com/patricia/notification/domain/model/enums/NotificationChannel.java
@@ -0,0 +1,7 @@
+package com.patricia.notification.domain.model.enums;
+
+public enum NotificationChannel {
+    PUSH,
+    IN_APP,
+    PUSH_AND_IN_APP
+}
---
+++ src/main/java/com/patricia/notification/domain/model/enums/NotificationType.java
@@ -0,0 +1,10 @@
+package com.patricia.notification.domain.model.enums;
+
+public enum NotificationType {
+    CONNECTION_REQUEST,
+    PARCHE_MESSAGE,
+    EVENT_REMINDER,
+    NEARBY_PARCHE,
+    ACHIEVEMENT_UNLOCKED,
+    PARCHE_INVITATION
+}
