7d2741af25216fe842d19dfaf921da82d2dcfc69
Cristian Guerrero <cristian.guerrero-b@mail.escuelaing.edu.co>
2026-05-07 10:07:47 -0500

    Feat: Implementacion Consumers

---
+++ src/main/java/com/patricia/notification/entrypoints/messaging/consumer/AuthNotificationConsumer.java
@@ -0,0 +1,55 @@
+package com.patricia.notification.entrypoints.messaging.consumer;
+
+import com.patricia.notification.domain.model.enums.NotificationType;
+import com.patricia.notification.domain.ports.in.SendNotificationUseCase;
+import com.patricia.notification.entrypoints.messaging.dto.OtpVerificationEventDto;
+import com.patricia.notification.entrypoints.messaging.dto.OtpResendEventDto;
+import com.patricia.notification.entrypoints.messaging.dto.PasswordResetEventDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class AuthNotificationConsumer {
+
+    private final SendNotificationUseCase sendNotificationUseCase;
+
+    @RabbitListener(queues = "${rabbitmq.queue.otp-verification}")
+    public void handleOtpVerification(OtpVerificationEventDto event) {
+        log.info("Evento recibido: OTP verificación para {}", event.getEmail());
+        sendNotificationUseCase.execute(
+                event.getEmail(),
+                NotificationType.EVENT_REMINDER,
+                "Código de verificación",
+                "Tu código de verificación es: " + event.getOtpCode(),
+                null
+        );
+    }
+
+    @RabbitListener(queues = "${rabbitmq.queue.otp-resend}")
+    public void handleOtpResend(OtpResendEventDto event) {
+        log.info("Evento recibido: OTP reenvío para {}", event.getEmail());
+        sendNotificationUseCase.execute(
+                event.getEmail(),
+                NotificationType.EVENT_REMINDER,
+                "Nuevo código de verificación",
+                "Tu nuevo código de verificación es: " + event.getOtpCode(),
+                null
+        );
+    }
+
+    @RabbitListener(queues = "${rabbitmq.queue.password-reset}")
+    public void handlePasswordReset(PasswordResetEventDto event) {
+        log.info("Evento recibido: recuperación de contraseña para {}", event.getEmail());
+        sendNotificationUseCase.execute(
+                event.getEmail(),
+                NotificationType.EVENT_REMINDER,
+                "Recuperación de contraseña",
+                "Tu código de recuperación es: " + event.getResetCode(),
+                event.getUserId()
+        );
+    }
+}
